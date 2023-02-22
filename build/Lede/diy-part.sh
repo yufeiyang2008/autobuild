5bf73e3fe5b5d17aa5cdd907dc311fb6384520f3
yufeiyang2008 <37684507+yufeiyang2008@users.noreply.github.com>
2023-02-22 22:35:01 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -4,14 +4,6 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
-cat >$NETIP <<-EOF
-uci set network.wan.proto='pppoe'
-uci set network.wan.username='051019840524'
-uci set network.wan.password='07271314'
-uci set network.wan.ifname='eth1'
-uci set network.wan6.ifname='eth1'
-EOF
-
 # 后台IP设置
 export Ipv4_ipaddr="192.168.123.1"            # 修改openwrt后台地址(填0为关闭)
 export Netmask_netm="255.255.255.0"         # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
